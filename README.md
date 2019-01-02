aaf181a292753e84f147fb9126cc71348be22e5e
279zlj <13414101644@163.com>
2019-01-02 18:31:00 +0800

    1.2

---
+++ README.md
@@ -0,0 +1,22 @@
+# model-sprite
+
+first
+
+npm install three
+npm install three-text2d
+
+second
+
+Same usage as ue-3d-model
+
+add new 
+spritefun    传参参数1
+spritetext   传参参数2
+tonealgin     参数位置1
+ttwoalgin     参数位置2
+tcolor     参数显示颜色
+
+You can modify it
+
+如果有别的要求可以按需修改model-mixin and utils
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <model-gltf :backgroundAlpha="bgAlpha"  :rotation="rotation"  @on-load="onLoad" :spritefun="safun" :spritetext="sastatic" :tonealgin="tonealgin"  :ttwoalgin="ttwoalgin" :tcolor="tcolor"  :position="where" :backgroundColor="bgColor" src="../../static/server/server2801.gltf" :cameraPosition="camera" :width="wid" :height="hei"></model-gltf>
+  </div>
+</template>
+
+<script>
+  import ModelGltf from "../assets/3d-text2d/vue-3d/model-gltf";
+export default {
+  name: 'HelloWorld',
+  components: {
+    ModelGltf,
+  },
+  data () {
+    return {
+      camera: {x: 0, y: 70, z: -280},
+      bgAlpha: .3,
+      bgColor: '#2E2245',
+      wid: 950,
+      hei: 443,
+      tcontent: '',
+      ttitle: '',
+      dosome: '',
+      sastatic:"CPU温度：30°C，风扇转速：300转",
+      safun: "显卡温度：25°C，阵列卡温度：28°C",
+      // sacard: this.$store.state.cardtem,
+      tonealgin:'bottomLeft',
+      ttwoalgin:'bottomRight',
+      tcolor:'#FFFFFF',
+      rotation: {
+        x: 0,
+        y: 0,
+        z: 0
+      },
+      where: {x: 0, y: 0, z: -20},
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
